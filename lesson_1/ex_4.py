f32505e89f69adfa226cb8847b4c0d91a818c83f
Eugene <evgeshka120@gmail.com>
2022-05-03 22:02:19 +0300

    exercise 4

---
+++ lesson_1/ex_4.py
@@ -0,0 +1,15 @@
+s_words = ['разработка', 'администрирование', 'protocol', 'standard']
+b_words = []
+
+for word in s_words:
+    b_words.append(word.encode('utf-8'))
+
+print(b_words)
+
+i = 0
+for word in b_words:
+    b_words[i] = word.decode('utf-8')
+    i += 1
+
+print(b_words)
+
