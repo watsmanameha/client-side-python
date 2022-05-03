a54048ddbd58f703a6641b70d59d36f228738d7f
Eugene <evgeshka120@gmail.com>
2022-05-03 21:53:59 +0300

    exercise 3

---
+++ lesson_1/ex_3.py
@@ -0,0 +1,8 @@
+s_words = ['attribute', 'класс', 'функция', 'type']
+
+for word in s_words:
+    print(word.encode('utf-8'))
+    # b'attribute' [x]
+    # b'\xd0\xba\xd0\xbb\xd0\xb0\xd1\x81\xd1\x81'
+    # b'\xd1\x84\xd1\x83\xd0\xbd\xd0\xba\xd1\x86\xd0\xb8\xd1\x8f'
+    # b'type' [x]
