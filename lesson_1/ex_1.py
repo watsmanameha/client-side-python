dedd34e8efcfcaf11f2ebddde096d07fa9ed6b61
Eugene <evgeshka120@gmail.com>
2022-05-03 21:50:12 +0300

    exercise 2

--- lesson_1/ex_1.py
+++ lesson_1/ex_1.py
@@ -3,9 +3,9 @@ s_words = ['разработка', 'сокет', 'декоратор']
 for word in s_words:
     print(type(word), word, '\n')
 
-words_in_b = {'\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430',
+words_in_b = ['\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430',
               '\u0441\u043e\u043a\u0435\u0442',
-              '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440'}
+              '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440']
 
 for word in words_in_b:
     print(type(word), word, '\n')
---
+++ lesson_1/ex_2.py
@@ -0,0 +1,7 @@
+s_words = ['class', 'function', 'method']
+
+words_in_b = ['\x63\x6c\x61\x73\x73', '\x66\x75\x6e\x63\x74\x69\x6f\x6e', '\x6d\x65\x74\x68\x6f\x64']
+
+for word in words_in_b:
+    print(type(word), word, len(word))
+
