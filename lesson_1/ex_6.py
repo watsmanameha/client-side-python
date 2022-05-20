cbc7d76d8d96322fa2891a6a3f01d8f8a3d0d647
watsmanameha <evgeshka120@gmail.com>
2022-05-20 12:14:39 +0300

    Fixed homework

--- lesson_1/ex_6.py
+++ lesson_1/ex_6.py
@@ -4,16 +4,16 @@
 
 import locale
 
-f_n = open('test_file.txt', 'w')
-
-f_n.write('сетевое программирование сокет декоратор')
-
-print(locale.getpreferredencoding())
-
-f_n.close()
+with open('test_file.txt', 'w', encoding='utf-8') as f_n:
+    f_n.write('сетевое программирование\n')
+    f_n.write('сокет\n')
+    f_n.write('декоратор\n')
+    print(f'Кодировка файла - {locale.getpreferredencoding()}')
+    # Кодировка файла - cp1251
 
 with open('test_file.txt', encoding='utf-8') as f_n:
     for el_str in f_n:
         print(el_str, end='')
-# UTF-8
-# сетевое программирование сокет декоратор
+# сетевое программирование
+# сокет
+# декоратор
